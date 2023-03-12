421628c73c59d8e660792e74b980867c8b12be28
Ana Castelhano <ana.f.castelhano@gmail.com>
2023-03-12 09:22:36 +0000

    aula 4 completa

--- aula4.py
+++ aula4.py
@@ -26,5 +26,15 @@ if not altura < 130:
 else:
     print("Não pode entrar")
 
+# o utilizador digita o ano e o programa responde se é bissesto ou n
+# um ano bissesto é divisivel por 4
+# exceto se é divisivel por 100
+# a menos que seja dividido por 400
+#(se for divisivel por 4 e por 100 não é bissesto a menos que seja divisivel por 400 tb)
+
+nome = "Ana Filipa Carrilho Castelhano"
+
+print(nome.lower())
+print(nome.count("a"))
 
 
